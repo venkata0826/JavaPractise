f3c7dd163be265968939149591d70ea97a0532dd
Harish <Harish@192.168.0.29>
2019-02-21 00:19:34 -0500

    Created a new Package Java Looping Concepts

---
+++ JavaPractise/src/JavaLoopingConcepts/ifelse.java
@@ -0,0 +1,18 @@
+package JavaLoopingConcepts;
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		
+		int a =10;
+		int b = 20;
+		
+		if(a<b){
+			System.out.println("The value of b is greater than a");
+		} else{
+			System.out.println("The value of a is less than b");
+		}
+
+	}
+
+}
