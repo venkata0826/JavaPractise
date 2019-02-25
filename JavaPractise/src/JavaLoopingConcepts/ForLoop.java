68d03fc9f5362904ad8379f50bcc95838bccf794
venkata0826 <venkata0826@gmail.com>
2019-02-24 22:25:01 -0500

    adding Forloop logic

---
+++ JavaPractise/src/JavaLoopingConcepts/ForLoop.java
@@ -0,0 +1,23 @@
+package JavaLoopingConcepts;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+		int i;
+		int j;
+		
+		for(i=0;i<10;i++){
+			System.out.println("the value of i is "+i);
+			for(j=10;j<1;j++){
+				System.out.println("the value of  is "+i);
+			}
+			
+		}
+		
+		
+		
+
+	}
+
+}
